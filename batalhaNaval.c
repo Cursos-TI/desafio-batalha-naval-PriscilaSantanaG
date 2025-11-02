c96d31da6e949830adef611a5a4319618b41bf0a
Priscila Santana <86635605+PriscilaSantanaG@users.noreply.github.com>
2025-11-02 12:09:07 +0000

    desafio 2

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,41 +1,57 @@
 #include <stdio.h>
 
-int main() {
-	// criando tabuleiro
-		int tabuleiro[10][10]={
-		{0,0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0,0}
-	};
-	// criando navios horizontal e vertical
-	int navioshorizontal[3]={1,2,3};
-	int naviosvertical[3]={3,2,1};
-	
-	for(int i = 0; i < 3; i++){
-		//a esturuta a seguir é uma matriz[x][y]
-		tabuleiro[0][navioshorizontal[i]]=3; //posicionando o navio substituindo a água linha
-		tabuleiro[naviosvertical[i]][5]=3; //posicionando o navio substituindo a água coluna
-	}
-	
-	// criando a estrutura de repetição para linha e coluna	
-	for(int lin = 0; lin < 10; lin++){
-		for(int col = 0; col < 10; col++){
-		printf("%d ", tabuleiro[lin][col]); 
-			
-		}
-		//quebra de linha para organizar o tabuleiro
-		printf("\n"); 
-	}
-	
-	
+int main()
+{
+    // criando tabuleiro nível novato
+    int tabuleiro[10][10] = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+    // criando navios horizontal e vertical
+    int navioshorizontal[3] = {1, 2, 3};
+    int naviosvertical[3] = {3, 2, 1};
 
-	
-	return 0;
+    for (int i = 0; i < 3; i++)
+    {
+        // a esturuta a seguir é uma matriz[x][y]
+        tabuleiro[0][navioshorizontal[i]] = 3; // posicionando o navio substituindo a água linha
+        tabuleiro[naviosvertical[i]][5] = 3;   // posicionando o navio substituindo a água coluna
+    }
+
+    // criando navios diagonais
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++) 
+        {   //navio diagonal 1 (esquerda para direita)
+            if (i == j && (i >= 4 && i <= 6))
+            {
+                tabuleiro[i][j] = 3;
+            }
+            // navio diagonal 2 (direita para esquerda)
+            if (i + j == 9 && (i >= 7 && i <= 9)) // teste soma dos índices
+            {                            
+                tabuleiro[i][j] = 3; // se for verdadeiro, posiciona o navio
+            }
+        }
+    }
+
+    // imprimindo o tabuleiro
+    for (int lin = 0; lin < 10; lin++)
+    {
+        for (int col = 0; col < 10; col++)
+        {
+            printf("%d ", tabuleiro[lin][col]);
+        }
+        // quebra de linha para organizar o tabuleiro
+        printf("\n");
+    }
+
+    return 0;
 }
